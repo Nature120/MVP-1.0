121a5b6d93dac1610e2715081a3d9b048349eff4
Vladyslav <vbochkovskyi@codempire.team>
2022-08-23 16:50:25 +0300

    added fonts,fonts-constants,react-native.config

--- src/screens/screen-test.tsx
+++ src/screens/screen-test.tsx
@@ -4,7 +4,22 @@ import { Text, View } from 'react-native';
 export const ScreenTest = () => {
   return (
     <View>
-      <Text>screen-test 2</Text>
+      <Text
+        style={{
+          fontFamily: 'NotoSans-Bold',
+          fontWeight: '400',
+          fontSize: 15,
+        }}>
+        The world’s first nature therapy app
+      </Text>
+      <Text
+        style={{
+          fontFamily: 'Lexend-Medium',
+          fontWeight: '400',
+          fontSize: 15,
+        }}>
+        The world’s first nature therapy app
+      </Text>
     </View>
   );
 };
