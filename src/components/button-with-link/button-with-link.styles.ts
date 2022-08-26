45e260979424a6e3f5e6b5900f560153d36f721d
EstivenGonzalez <sgonsales@codempire.team>
2022-08-26 18:02:22 +0300

    feature/button

---
+++ src/components/button-with-link/button-with-link.styles.ts
@@ -0,0 +1,34 @@
+import { COLOR } from '@theme/colors';
+import { FONTS } from '@theme/fonts';
+import styled from 'styled-components/native';
+import { IStyledButtonWithLinkProps } from './button-with-link.typings';
+
+export const StyledButtonWithLink = {
+  ButtonWithLink: styled.View<IStyledButtonWithLinkProps>`
+    margin-horizontal: ${props => (props.isWithMagrin ? '24px' : 0)};
+    align-items: center;
+    margin-bottom: ${props => (props.marginBottom ? props.marginBottom : 0)}px;
+  `,
+
+  SubButton: styled.View`
+    margin-top: 24px;
+    flex-direction: row;
+  `,
+
+  TouchableText: styled.Text`
+    margin-left: 8px;
+    font-family: ${FONTS.family.boldAcumin};
+    font-weight: ${FONTS.weight.bold};
+    font-size: ${FONTS.size.xSmall};
+    line-height: 17px;
+    color: ${COLOR.subheading};
+  `,
+
+  SubButtonText: styled.Text`
+    font-family: ${FONTS.family.lightBoreal};
+    font-weight: ${FONTS.weight.regular};
+    font-size: ${FONTS.size.xSmall};
+    line-height: 18px;
+    color: ${COLOR.subheading};
+  `,
+};
---
+++ src/components/button-with-link/button-with-link.typings.ts
@@ -0,0 +1,18 @@
+import { GestureResponderEvent } from 'react-native';
+
+import { IButtonProps } from '@components/button/button.typings';
+
+export interface IButtonWithLinkProps
+  extends IButtonProps,
+    IStyledButtonWithLinkProps {
+  bottomText?: string;
+  routeText: string;
+  onTextPressNavigateTo: string;
+  onButtonPressNavigateTo?: string;
+  onTextPress?: (event: GestureResponderEvent) => void;
+}
+
+export interface IStyledButtonWithLinkProps {
+  isWithMagrin?: boolean;
+  marginBottom?: 24 | 36;
+}
