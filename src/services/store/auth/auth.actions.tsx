6a47c32bd4e340848df64fe28add15a5c5bb3a3b
Vladyslav <vbochkovskyi@codempire.team>
2022-09-08 11:51:55 +0300

    updated redux and api service

--- src/services/store/auth/auth.actions.tsx
+++ src/services/store/auth/auth.actions.tsx
@@ -1,3 +1,8 @@
 import { createAction } from '@reduxjs/toolkit';
 
-export const test = createAction('auth/test');
+import { IUser } from './auth.interface';
+
+export const signUp = createAction<IUser>('auth/signUp');
+export const signIn = createAction<IUser>('auth/signIn');
+export const isAuthenticated = createAction<boolean>('auth/isAuthenticated');
+export const signOut = createAction<null>('auth/signOut');
