0f404d7c467decbfe87e432ddf3af75b91ed6d8c
EstivenGonzalez <sgonsales@codempire.team>
2022-09-09 14:51:58 +0300

    bugfix/sign-up-ios

--- src/navigation/root.navigator.tsx
+++ src/navigation/root.navigator.tsx
@@ -55,7 +55,9 @@ export const RootNavigator = () => {
 
   if (authenitcated) {
     return (
-      <MainStack.Navigator initialRouteName={isFirstLaunch ? APP_ROUTES.start.onBoard : APP_ROUTES.main.home}>
+      <MainStack.Navigator
+        screenOptions={screenOptions}
+        initialRouteName={isFirstLaunch ? APP_ROUTES.start.onBoard : APP_ROUTES.main.home}>
         {ON_BOARD_ROUTES.map(({ component, name }) => (
           <StartStack.Screen key={name} name={name} component={component} />
         ))}
