6bb5b1c00153876ba9249f3ec9e02e8f7fd9d51b
EstivenGonzalez <sgonsales@codempire.team>
2022-10-19 18:53:10 +0300

    feature/sentry-setup

--- src/screens/_main/main.tsx
+++ src/screens/_main/main.tsx
@@ -4,12 +4,18 @@ import { SafeAreaProvider } from 'react-native-safe-area-context';
 import SplashScreen from 'react-native-splash-screen';
 import { Provider } from 'react-redux';
 import { NavigationContainer } from '@react-navigation/native';
+import * as Sentry from '@sentry/react-native';
 import { persistStore } from 'redux-persist';
 import { PersistGate } from 'redux-persist/integration/react';
 
 import { RootNavigator } from '@navigation/root.navigator';
 
 import { store } from '@services/store';
+
+Sentry.init({
+  dsn: 'https://f5cb3cdc8e2e47b18e73dc07521d4f51@o4504010858889216.ingest.sentry.io/4504010861051904',
+  tracesSampleRate: 1.0,
+});
 
 export const Main = () => {
   const persistor = persistStore(store);
@@ -31,3 +37,5 @@ export const Main = () => {
     </Provider>
   );
 };
+
+export default Sentry.wrap(Main);
