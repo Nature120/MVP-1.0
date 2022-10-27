2805086be14fd9bd358a9cb8a980e064030fa7ba
Vladyslav <vbochkovskyi@codempire.team>
2022-10-27 17:33:37 +0300

    clear timer before logout

--- src/services/store/timer/timer.reducer.tsx
+++ src/services/store/timer/timer.reducer.tsx
@@ -1,13 +1,16 @@
 import { combineReducers, createReducer } from '@reduxjs/toolkit';
 
+import * as actionAuth from '../auth/auth.actions';
 import * as action from './timer.actions';
 
 const startDate = createReducer(null, {
   [action.startDate.type]: (_, { payload }) => payload,
+  [actionAuth.signOut.type]: () => null,
 });
 
 const seconds = createReducer(null, {
   [action.seconds.type]: (_, { payload }) => payload,
+  [actionAuth.signOut.type]: () => null,
 });
 
 const isActiveTimer = createReducer(true, {
