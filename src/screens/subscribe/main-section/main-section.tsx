b69ede5fe5d3c740deb7480e4004625e72ecaf8d
Vladyslav <vbochkovskyi@codempire.team>
2023-01-10 13:26:39 +0200

    refactored main section in subscription screen

--- src/screens/subscribe/main-section/main-section.tsx
+++ src/screens/subscribe/main-section/main-section.tsx
@@ -1,75 +1,18 @@
-import React, { useCallback, useEffect, useState } from 'react';
+import React from 'react';
 import { View } from 'react-native';
-import Purchases, { PACKAGE_TYPE, PurchasesOffering, PurchasesPackage } from 'react-native-purchases';
-import { useDispatch } from 'react-redux';
-import { useNavigation } from '@react-navigation/native';
+import { PurchasesPackage } from 'react-native-purchases';
 
 import { Button } from '@components/atoms/button';
 import { Icon } from '@components/atoms/icon';
+import { useMainSectionState } from './main-section.state';
 import { OfferItem } from './offer-item/offer-item';
-
-import { useStoreSubscription } from '@services/hooks/subscription-store';
-import { loading } from '@services/store/auth/auth.actions';
 
 import { arrayBenefits } from './main-section.constants';
 
 import { MainSectionStyled as Styled } from './main-section.styles';
 
 export const MainSection = () => {
-  const [offers, setOffers] = useState<PurchasesOffering | null>(null);
-  const [offer, setOffer] = useState<PurchasesPackage | null>(null);
-  const [checkedButton, setCheckedButton] = useState<boolean>(false);
-  const [checkedOfferName, setCheckedOfferName] = useState<PACKAGE_TYPE | null>(null);
-  const { goBack } = useNavigation();
-  const dispatch = useDispatch();
-  const { storeSubscription } = useStoreSubscription();
-
-  useEffect(() => {
-    fetchOfferings();
-  }, []);
-
-  const buyPackage = async () => {
-    if (!offer) {
-      return;
-    }
-
-    try {
-      const { customerInfo } = await Purchases.purchasePackage(offer);
-
-      const { premium } = customerInfo.entitlements.active;
-      if (typeof premium !== 'undefined') {
-        await storeSubscription(premium.productIdentifier);
-
-        goBack();
-        return;
-      }
-      //End loaging screen
-      dispatch(loading(false));
-    } catch (e: any) {
-      if (!e.userCancelled) {
-        console.log('error', e);
-      }
-    }
-  };
-
-  const onPressOffer = useCallback(
-    (pack: PurchasesPackage) => {
-      const { packageType } = pack;
-      !checkedButton && setCheckedButton(true);
-      setCheckedOfferName(packageType);
-      setOffer(pack);
-    },
-    [offer, checkedOfferName, setCheckedButton],
-  );
-
-  const fetchOfferings = async () => {
-    try {
-      const { current } = await Purchases.getOfferings();
-      current !== null && setOffers(current);
-    } catch (error) {
-      console.log('error', error);
-    }
-  };
+  const { onPressOffer, buyPackage, offers, checkedButton, checkedOfferName } = useMainSectionState();
 
   return (
     <Styled.Container>
