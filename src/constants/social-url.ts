126f613e530c99a241a74ba3f262464ff032a99a
Vladyslav <vbochkovskyi@codempire.team>
2023-02-09 17:02:17 +0200

    changed social url's

--- src/constants/social-url.ts
+++ src/constants/social-url.ts
@@ -1,3 +1,3 @@
-export const TERMS = 'https://www.natureonetwenty.com/app-terms-of-service';
-export const PRIVACY = 'https://www.natureonetwenty.com/privacy';
+export const TERMS = 'https://nature120.app/app-terms-of-service';
+export const PRIVACY = 'https://nature120.app/privacy';
 export const INSTAGRAM = 'https://www.instagram.com/nature120.app/';
