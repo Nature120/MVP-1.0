c6d959191bbefa463d2eb22326112b6dca930163
Vladyslav <vbochkovskyi@codempire.team>
2023-02-10 13:28:35 +0200

    fixed teacher section in immersion screen

--- src/screens/immersions/immersions.state.ts
+++ src/screens/immersions/immersions.state.ts
@@ -3,7 +3,7 @@ import { useDispatch, useSelector } from 'react-redux';
 import { useNavigation } from '@react-navigation/native';
 
 import { getRecentLibraries, sortLibraries } from './immersions.utils';
-import { databaseRef } from '@services/api.service';
+import { databaseRef, getTeacher } from '@services/api.service';
 import { useSetDefaultTimer } from '@services/hooks/setDefaultTimer';
 import * as action from '@services/store/auth/auth.actions';
 import { getUserInfo } from '@services/store/auth/auth.selectors';
@@ -26,7 +26,20 @@ export const useImmersions = () => {
   const getRecentLibrariesFromDB = useCallback(async () => {
     setIsLoading(true);
     const allLibraries = await databaseRef('Practise library').where('title', 'in', recentLibrariesTitles).get();
-    const librariesData = allLibraries.docs.map(lib => lib.data() as IPracticeLibrary);
+    const librariesData = (await Promise.all(
+      allLibraries.docs.map(async lib => {
+        try {
+          const data = lib.data();
+          if (data.teacher) {
+            const teacher = await getTeacher(data.teacher);
+            data.teacher = teacher;
+          }
+          return data;
+        } catch (error) {
+          console.log('error', error);
+        }
+      }),
+    )) as IPracticeLibrary[];
 
     const sortedLibraries = sortLibraries(recentLibrariesTitles, librariesData);
 
